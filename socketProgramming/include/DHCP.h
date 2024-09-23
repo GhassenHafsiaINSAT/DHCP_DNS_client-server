44b8d75ec42172fef8db02837505e94226df8936
HAFSIA GHASSEN <110825502+GhassenHafsiaINSAT@users.noreply.github.com>
2024-09-23 18:48:06 +0100

    working with checkout_ip and init_pool functionsip

--- socketProgramming/include/DHCP.h
+++ socketProgramming/include/DHCP.h
@@ -1,31 +1,9 @@
 #include <stdio.h>
 #include <time.h>
+#include "common.h"
 
 
-
-typedef struct 
-{
-    // Tracks if the IP is currently assigned to a device or not
-    // Links the IP  to a specific device 
-    
-    char ip_address[16]; 
-    int is_assinged;        // 1 if assigned 0 else
-    char device_id[50];     // device ti which the ip address is assigned 
-
-} IPAddress;
-
-
-typedef struct 
-
-{
-    // Represents the whole set of IP addresses managed by the server
-
-    IPAddress *pool;    // pointer to an array of IP address structure 
-    int pool_size;      // number of IP addresses in the pool 
-
-} IPAddressPool;
-
-
+#define MAX_CLIENT 20 
 typedef struct 
 {   
     // when client requests an IP address, the server checks if the client already has active lease
@@ -35,16 +13,34 @@ typedef struct
     char ip_address[16]; 
     time_t lease_start; 
     time_t lease_end; 
-};
+} Client;
+
+typedef struct 
+
+{
+    // Represents the whole set of IP addresses managed by the server
+
+    char IP_address[20][INET_ADDRSTRLEN];    // pointer to an array of IP address structure 
+    int pool_size;      // number of IP addresses in the pool 
+    char allocated[20]; 
+    Client clients[MAX_CLIENT]; 
+    int client_count; 
+} IPAddressPool;
+
+
+
+
+unsigned int ip_to_int(const char *ip); 
+
+void int_to_ip(unsigned int ip, char *buffer); 
 
 // Initializing the IP pool, with defined range of addresses and mark all IPs as available initially
 void init_pool(IPAddressPool *pool, const char *start_ip, const char *end_ip); 
 
 
 // Check the IP pool for an available IP and assing it temporarily
-int checkout_ip(IPAddressPool *pool, const char *device_id, char *assigned_ip); 
-    // return assigned ip variable 
+int checkout_ip(IPAddressPool *pool, char *mac_address, char *assigned_ip); 
 
 // marks the ip address as available 
-int releasing_ip(IPAddressPool *pool, const char *device_id); 
+int releasing_ip(IPAddressPool *pool, char *mac_address); 
 
